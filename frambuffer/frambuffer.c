b36fa7cf4ae2ccdf7a565fdf6fbee3cb79592f8f
imdwq <xm.imdwq@gmail.com>
2016-10-25 17:20:07 +0800

    修改：     frambuffer/Makefile 	修改：     frambuffer/fbrun 	修改：     frambuffer/fbrun.c 	修改：     frambuffer/frambuffer.c 	修改：     frambuffer/frambuffer.h 	修改：     frambuffer/libfb.so

--- frambuffer/frambuffer.c
+++ frambuffer/frambuffer.c
@@ -44,16 +44,73 @@ unsigned char* fb_init()
 	return ((unsigned char *)mmap(NULL, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
 }
 
-void draw_point(unsigned char *buff, int x, int y,unsigned int red_p, unsigned int green_p, unsigned int blue_p)
+void make_color(color_8 *out, unsigned int red, unsigned int green, unsigned int blue, unsigned int alpha)
+{
+	out->red = red;
+	out->green = green;
+	out->blue = blue;
+	out->alpha = alpha;
+}
+
+void draw_point(unsigned char *buff, int x, int y, color_8 color)
 {
 	unsigned int offset = (y * xres + x) * bits_per_pixel / BIT_TO_BYTE;
-	*(unsigned char *)(buff + offset + red.offset / BIT_TO_BYTE) = red_p;
-	*(unsigned char *)(buff + offset + green.offset / BIT_TO_BYTE) = green_p;
-	*(unsigned char *)(buff + offset + blue.offset / BIT_TO_BYTE) = blue_p;
+	*(unsigned char *)(buff + offset + red.offset / BIT_TO_BYTE) = color.red;
+	*(unsigned char *)(buff + offset + green.offset / BIT_TO_BYTE) = color.green;
+	*(unsigned char *)(buff + offset + blue.offset / BIT_TO_BYTE) = color.blue;
 
 }
 
-void draw_block(unsigned char *buff, int xmin,int xmax, int ymin, int ymax, unsigned int red_i, unsigned int green_i, unsigned int blue_i)
+void draw_func(unsigned char *buff, IntFunc func, int xmin, int xmax, color_8 color, int x0, int y0)
+{
+	int x, y, tmp;
+	if (xmin > xmax)
+	{
+		tmp = xmin;
+		xmin = xmax;
+		xmax = tmp;
+	}
+	for(tmp = xmin; tmp < xmax; tmp++)
+	{
+		y = y_axis_trans((*func)(tmp) + y0);
+		x = tmp + x0;
+		draw_point(buff, x, y, color);
+	}
+}
+
+void draw_func_0(unsigned char *buff, IntFunc func, int xmin, int xmax, color_8 color)
+{
+	draw_func(buff, func, xmin, xmax, color, 0 , 0);
+}
+
+void draw_line(unsigned char *buff, int xmin, int xmax, int ymin, int ymax, color_8 color)
+{
+	int tmp, x, y;
+	double ratio;
+
+	if (xmin > xmax)
+	{
+		tmp = xmin;
+		xmin = xmax;
+		xmax = tmp;
+	}
+	if (ymin > ymax)
+	{
+		tmp = ymin;
+		ymin = ymax;
+		ymax = tmp;
+	}
+
+	ratio = (double)(ymax - ymin) / (double)(xmax - xmin);
+
+	for(x = xmin; x < xmax; x++)
+	{
+		y = (unsigned int)(ratio * (x - xmin) + ymin);
+		draw_point(buff, x, y, color);
+	}
+}
+
+void draw_block(unsigned char *buff, int xmin, int xmax, int ymin, int ymax, color_8 color) 
 {
 	int tmp;
 	if (xmin > xmax)
@@ -72,7 +129,7 @@ void draw_block(unsigned char *buff, int xmin,int xmax, int ymin, int ymax, unsi
 	for (int j = ymax; j > ymin; j--)
 	{
 		for (int i = xmin; i < xmax; i++)
-			draw_point(buff, i, j, red_i, green_i, blue_i);
+			draw_point(buff, i, j, color);
 	}
 }
 
@@ -86,3 +143,28 @@ void release_fb(unsigned char* buff)
 	munmap(buff, screensize); 
 }
 
+void zero_func_trans(int *x, int *y, int x0, int y0)
+{
+	*y = *y - y0;
+	*x = *x - x0;
+}
+
+int y_axis_trans(int y)
+{
+	return yres - 1 - y;
+}
+
+DrawFunc func_line(int x)
+{
+	return x;
+}
+
+DrawFunc func_parabola(int x)
+{
+	return x*x;
+}
+
+DrawFunc func_parabola_0(int x)
+{
+	return x*x;
+}
--- frambuffer/frambuffer.h
+++ frambuffer/frambuffer.h
@@ -8,20 +8,31 @@ typedef struct fb_var_screeninfo vinfo;
 typedef struct fb_bitfield color;
 typedef struct
 {
-	unsigned int red_8;
-	unsigned int green_8;
-	unsigned int blue_8;
+	unsigned int red;
+	unsigned int green;
+	unsigned int blue;
+	unsigned int alpha;
 }color_8;
 
+typedef int DrawFunc;
+typedef DrawFunc (*IntFunc)(int);
 
-void draw_point(unsigned char* buff, int x,int y , unsigned int red, unsigned int green, unsigned int blue);
+void make_color(color_8 *out,unsigned int red, unsigned int green, unsigned int blue, unsigned int alpha);
 
-void draw_block(unsigned char* buff, int xmin,int xmax, int ymin, int ymax, unsigned int red, unsigned int green, unsigned int blue);
+void draw_point(unsigned char* buff, int x, int y, color_8 color);
+void draw_func(unsigned char *buff, IntFunc func, int xmin, int xmax, color_8 color, int x0, int y0);
+void draw_func_0(unsigned char *buff, IntFunc func, int xmin, int xmax, color_8 color);
+void draw_line(unsigned char *buff, int xmin, int xmax, int ymin, int ymax, color_8 color);
+void draw_block(unsigned char* buff, int xmin,int xmax, int ymin, int ymax, color_8 color);
 
 unsigned char *fb_init();
+void release_fb(unsigned char* buff);
 
 void wipe_screen(unsigned char* buff);
 
-void release_fb(unsigned char* buff);
+int y_axis_trans(int y);
+void zero_func_trans(int *x, int *y, int x0, int y0);
 
+DrawFunc func_line(int x);
+DrawFunc func_parabola(int x);
 #endif
